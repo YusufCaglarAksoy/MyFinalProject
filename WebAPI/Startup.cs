34def07901d75e0b72add8c62178669fc8f9b22d
Yusuf Çağlar Aksoy <77413549+YusufCaglarAksoy@users.noreply.github.com>
2021-03-24 19:51:24 +0300

    20. Gün

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -55,7 +55,10 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
